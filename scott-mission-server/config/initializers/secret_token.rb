348e79034a7b8fec737929099b10497cdf92a305
Marius Butuc <marius.butuc@ca.ibm.com>
2011-11-10 10:21:37 -0500

    Rails reading Feeds

---
+++ scott-mission-server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScottMissionServer::Application.config.secret_token = 'fb31d760f91dc931f5f50c6ff06359bc39ac180a72a60947c42a9bddf42bfa5865695380bad4c7d9ef6532094ae7e5da9ada316b94620d3e7814eaf6012ba938'
